3cdb7b5aed41603a6e18f31a8d680f8dd298d6f5
tabaressara <tabaressara1110@javerianacali.edu.co>
2023-05-22 09:12:08 -0500

    Inicio streamlit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import view.zooView as viewZoo
+import streamlit as st
 
 if __name__ == '__main__':
+    st.set_page_config(
+        page_title = "Zoologico",
+        layout = "wide"
+    )
     sara = viewZoo.vistaZoo()
     sara.mostrar_menu()
